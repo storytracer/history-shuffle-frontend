c068ea59bd8ee57cfa5b9583c99ed91a8a779626
Sebastian Majstorovic <canbuffi@gmail.com>
2021-02-22 14:00:29 +0100

    Removed selection buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,18 +4,6 @@
       <header class="has-text-centered">
         <h1 class="title is-1">Temopedia Shuffle</h1>
         <h2 class="subtitle is-3">Shuffle through Digital History collections</h2>
-        <div class="level has-text-centered">
-          <div class="level-item">
-            <div class="field has-addons">
-              <p class="control">
-                <b-button type="is-danger" outlined><span>Europeana</span></b-button>
-              </p>
-              <p class="control">
-                <b-button type="is-danger" outlined><span>World Digital Library</span></b-button>
-              </p>
-            </div>
-          </div>
-        </div>
       </header>
       <SourceCard v-for="i in cards" :key="i" />
       <div class="has-text-centered">
